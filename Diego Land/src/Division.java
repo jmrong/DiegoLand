57024ffa16bbc93820f5768536d9a63304133e09
Anish_CTY <CTYSession1@DANA181-10-9715.skidmore.edu>
2017-07-11 20:30:42 -0400

    part a division class

---
+++ Diego Land/src/Division.java
@@ -0,0 +1,13 @@
+
+public class Division {
+	private Unit[][] units;
+	private String name;
+	
+	public Division(Unit[][] units, String name)
+	{
+		this.units = units;
+		this.name = name;
+	}
+	
+	
+}
